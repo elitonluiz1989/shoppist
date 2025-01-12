141560bd966edfb6afd3450d8ebefdaef7dbcc0c
elitonluiz1989 <elitonluiz1989@gmail.com>
2025-01-11 23:20:24 -0400

    Created a new type of Result

---
+++ Application/Shared/Results/BaseResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Immutable;
+
+namespace Application.Shared.Results;
+
+public abstract class BaseResult<TResult>(ImmutableArray<ErrorResult> errors)
+    where TResult : BaseResult<TResult>
+{
+    public ImmutableArray<ErrorResult> Errors { get; } = errors;
+
+    public bool IsSuccess => Errors.Length == 0;
+
+    public bool IsFailure => !IsSuccess;
+
+    public static TResult CreateFailure(ImmutableArray<ErrorResult> errors) =>
+        (Activator.CreateInstance(typeof(TResult), errors) as TResult)!;
+
+    public static TResult CreateFailure(ErrorResult error) => CreateFailure([error]);
+}
--- Application/Shared/Results/Result.cs
+++ Application/Shared/Results/Result.cs
@@ -2,30 +2,25 @@
 
 namespace Application.Shared.Results;
 
-public class Result<TValue>
+public class Result(ImmutableArray<ErrorResult> errors) : BaseResult<Result>(errors)
 {
-    private Result(ImmutableArray<ErrorResult> errors)
+    public Result() : this(ImmutableArray<ErrorResult>.Empty)
     {
-        IsSuccess = false;
-        Errors = errors;
     }
 
-    private Result(TValue? value)
+    public static Result CreateSuccess() => new();
+}
+
+public class Result<TValue>(ImmutableArray<ErrorResult> errors) : BaseResult<Result<TValue>>(errors)
+{
+    public Result(TValue? value) : this(ImmutableArray<ErrorResult>.Empty)
     {
         Value = value;
-        IsSuccess = true;
-        Errors = [];
     }
 
     public TValue? Value { get; private set; }
-    public ImmutableArray<ErrorResult> Errors { get; init; }
-
-    public bool IsSuccess { get; set; }
-
-    public bool IsFailure => !IsSuccess;
 
     public static Result<TValue> CreateSuccess(TValue value) => new(value);
+
     public static Result<TValue> CreateSuccess() => new(default(TValue));
-    public static Result<TValue> CreateFailure(ImmutableArray<ErrorResult> errors) => new(errors);
-    public static Result<TValue> CreateFailure(ErrorResult error) => new([error]);
 }
